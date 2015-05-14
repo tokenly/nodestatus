06a390169ed2820cc853dbeb545c82c12e15742e
Devon Weller <dweller@devonweller.com>
2015-05-14 13:11:50 -0500

    added index

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Node Status</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Node Status</h1>
+    <div>
+<?php echo "Hello World from PHP [".date("Y-m-d H:i:s")."]"; ?>
+    </div>
+
+    <div><pre>
+<?php echo "SERVER: ".json_encode($_SERVER, 192); ?>
+    </pre></div>
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>
