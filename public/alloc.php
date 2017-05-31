e2e9b2169cb83e1dd451b63036f21d06bc734ee9
Devon Weller <devon@tokenly.co>
2017-05-31 16:15:31 -0500

    added alloc

---
+++ public/alloc.php
@@ -0,0 +1,4 @@
+<?php 
+
+echo "NOMAD_ALLOC_INDEX: ".getenv('NOMAD_ALLOC_INDEX')."\n";
+echo "   NOMAD_ALLOC_ID: ".getenv('NOMAD_ALLOC_ID')."\n";
--- public/delay.php
+++ public/delay.php
@@ -6,4 +6,3 @@ sleep($delay);
 $id = isset($_GET['id']) ? $_GET['id'] : 'none';
 echo "Request {$id} was delayed for {$delay} seconds.\n";
 
-?>
